7a1bc5d546894ec913284182c5fd1efc3fd81f86
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-26 15:04:19 +0100

    setup routes and add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Layouts/Navbar";
+import Home from './pages/Home'
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
